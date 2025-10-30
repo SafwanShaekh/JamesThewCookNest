397dcc7296e462a2197bf0fda5b4b63d5b675c41
Safwan Sheikh <safwanshaekh786@gmail.com>
2025-10-31 01:34:55 +0500

    Add project files.

---
+++ JamesThewCookNest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JamesThewCookNest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
